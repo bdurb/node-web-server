f8d4de583a024c93028cbd8c6e97b1f4dd8b79ed
bdurb <brian@rpmgreenville.com>
2018-07-15 14:59:14 -0700

    set up a few test routes

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.send('<h1>Hello Express</h1>')
 });
 
+app.get('/about', (req, res) => {
+  res.send('<h1>About Page</h1>')
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
 app.listen(3000);
