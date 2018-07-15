99fa8ee2ff48df468eed0ad9f697c0e993e66583
bdurb <brian@rpmgreenville.com>
2018-07-15 16:04:42 -0700

    set up hbs files for home and about page

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
-
+const hbs = require('hbs');
 const app = express();
 
+
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express</h1>')
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Hello, welcome to the things'
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>About Page</h1>')
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
